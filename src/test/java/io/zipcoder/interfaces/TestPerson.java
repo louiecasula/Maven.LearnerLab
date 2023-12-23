29102735b47da44540b6fea46d03a893010938e1
louie <louiecasula@gmail.com>
2023-12-23 10:30:56 -0500

    TestPerson test class made

--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,23 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
 
+    @Test
+    public void testConstructor(){
+        Person person = new Person(0L, "Leuth");
+
+        Assert.assertNotNull(person);
+    }
+
+    @Test
+    public void testSetName(){
+        Person person = new Person(0L, "Leuth");
+        String expected = "Sleuth";
+        person.setName(expected);
+
+        Assert.assertEquals(expected, person.getName());
+    }
 }
